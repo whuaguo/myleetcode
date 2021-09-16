39c74db4eafabc52fc83794ee45cac79299fc488
whuaguo <whuaguo@gmail.com>
2021-09-16 22:45:48 +0800

    c

--- 21.合并两个有序链表.c
+++ 21.合并两个有序链表.c
@@ -15,7 +15,22 @@
 
 
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+    struct ListNode *p1 = l1;
+    struct ListNode *p2 = l2;
+    struct ListNode *retp = NULL;
+    struct ListNode *op = NULL;
 
+    if (!p1 || !p2 ) {
+        return p1?p1:p2;
+    }
+
+    retp = (p1->val >p2->val)? p2:p1; 
+
+    while (p1 || p2) {
+
+    }
+
+    return retp;
 }
 // @lc code=end
 
