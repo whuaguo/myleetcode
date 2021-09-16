fe0e3f51f530bdac0ffabc23e9932606b938565c
whuaguo <whuaguo@gmail.com>
2021-09-16 22:31:32 +0800

    c

--- 20.有效的括号.c
+++ 20.有效的括号.c
@@ -6,9 +6,48 @@
 
 // @lc code=start
 
+bool is_paired(char a, char b) {
+    return ((a == '(') && (b == ')') || 
+            (a == '[') && (b == ']') ||
+            (a == '{') && (b == '}'));
+}
+
+bool is_left(char a) {
+    return (a=='(') || (a=='[') || (a=='{');
+}
 
 bool isValid(char * s){
+    char stack[100001];
+    int len = strlen(s);
+    int stack_len = 0;
 
+    //奇数长度，肯定false
+    if (len & 1) {
+        return false;
+    }
+
+    for (int i = 0; i < len; i++ ) {
+        if (is_left(s[i])) {
+            //入栈
+            stack[stack_len++] = s[i];
+        } else {
+            //栈空，false
+            if (stack_len == 0) {
+                return false;
+            }
+
+            if (!is_paired(s[i], stack[stack_len - 1])) {
+                stack_len --;
+                return false;
+            }
+        }
+    }
+
+    if (stack_len != 0) {
+        return false;
+    }
+
+    return true;
 }
 // @lc code=end
 
