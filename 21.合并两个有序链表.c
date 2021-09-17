a6a6673fa75198580244135d647c4ba80d0e5f00
whuaguo <whuaguo@gmail.com>
2021-09-17 09:33:14 +0800

    c

--- 21.合并两个有序链表.c
+++ 21.合并两个有序链表.c
@@ -24,10 +24,35 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
         return p1?p1:p2;
     }
 
-    retp = (p1->val >p2->val)? p2:p1; 
+    //get the small one head, it will be the return node.
+    retp = (p1->val >p2->val)? p2:p1;
 
+    //the last one node;
+    op = retp;
+
+    //move out the head
+    if (retp == p1) {
+        p1 = p1->next;
+    } else {
+        p2 = p2->next;
+    }
+
+    //add the small nodes in the p1&p2 to the op one by one.
     while (p1 || p2) {
+        //p1 or p2 is empty, add all non-empty one to op
+        if (!p1 || !p2) {
+            op->next = p1?p1:p2;
+            break;
+        }
 
+        if (p1->val <= p2->val) {
+            op->next = p1;
+            p1 = p1->next;
+        } else {
+            op->next = p2;
+            p2 = p2->next;
+        }
+        op = op->next;
     }
 
     return retp;
