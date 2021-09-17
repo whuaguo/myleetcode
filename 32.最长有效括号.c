6e4217b9ce22238703684f6b62873b0c61738798
whuaguo <whuaguo@gmail.com>
2021-09-17 23:02:56 +0800

    c

--- 32.最长有效括号.c
+++ 32.最长有效括号.c
@@ -9,58 +9,52 @@
 
 int longestValidParentheses(char * s){
     int len = strlen(s);
+    short *pvalue;
+    short *pindex;
 
     if (len < 2) {
         return 0;
     }
 
-    int value = 0;
-    int mlen = 0;
-    int rmlen = 0;
-    int zlen = 0;
-    int llen = 0;
+    pvalue = malloc (sizeof(short) * len);
+    if (pvalue == NULL) {
+        return 0;
+    }
 
-    int max = 0;
-    int cmax = 0
+    pindex = malloc (sizeof(short) * len);
+    if (pindex == NULL) {
+        free(pvalue);
+        return 0;
+    }
 
-    for (int idx = 0; idx < len; idx ++) {
+    short value = 0;
+    short rmlen = 0;
+    short zlen = 0;
+    for (short idx = 0; idx < len; idx ++) {
         if (s[idx] == '(') {
             value ++;
-            mlen ++;
-            llen ++;
-        } else {
-            if (value > 0) {
-                value --;
-                mlen ++;
-                llen = 0;
+            pvalue[idx] = value;
 
-                if (value == 0) {
-                    zlen += mlen;
-                    mlen = 0;
-                } else {
-                    //已经有配对，更新一下最大值
-                    if (mlen - value > zlen) {
-                        zlen = mlen - value;
-                    }
-                }
-            } else {
+            if ((idx > 1) && (s[idx -1] == ')')) {
+                if (pvalue[idx - 1] >= 0) {
+                    zlen = idx -1 - pindex[idx -1];
+                } 
                 if (rmlen < zlen) {
                     rmlen = zlen;
                 }
-                mlen = 0;
-                zlen = 0;
             }
+        } else {
+            if (value > 0) {
+                value --;
+                pvalue[idx] = value;
+            } else {
+                pvalue[idx] = -1;
+            } 
         }
     }
 
-    if (zlen > rmlen) {
-        rmlen = zlen;
-    }
-
-    if (mlen - value > rmlen) {
-        rmlen = mlen - value;
-    }
-
+    free(pvalue);
+    free(pindex);
     return rmlen;
 }
 // @lc code=end
