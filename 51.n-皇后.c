8671ad0235009e20c00c80fc94f75bb56c867fce
whuaguo <whuaguo@gmail.com>
2021-09-28 20:04:55 +0800

    c

--- 51.n-皇后.c
+++ 51.n-皇后.c
@@ -13,20 +13,57 @@
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 
-bool findQSolution(char **existQ, int totalSize, int leftSize) {
+
+unsigned char qs[100][10] = {0};
+unsigned char qsn = 0;
+
+static bool nQconflict(unsigned char *steps, 
+                       unsigned char totalSteps, 
+                       unsigned char existSteps, 
+                       unsigned char key) {
+    for (unsigned char i = 0; i < existSteps; i++) {
+        if ((steps[i] == key) || (steps[i] - i == existSteps - key)) {
+            return false;
+        }
+    }
 
     return true;
 }
 
-char qs[100][9][9] = {0};
-int qsn = 0;
+void findQSolution(unsigned char *steps, unsigned char totalSteps, unsigned char existSteps) {
+    if (totalSteps == existSteps + 1) {
+        //find one solution
+        memcpy(qs[qsn], steps, totalSteps);
+        qsn ++;
+        return;
+    }
+
+    for (unsigned char i = 0; i < totalSteps - 1; i ++) {
+        if (nQconflict(steps, totalSteps, existSteps, i)) {
+            steps[existSteps] = i;
+            findQSolution(steps, totalSteps, existSteps+1);
+            steps[existSteps] = 0;
+        }
+    }
+
+    return;
+}
+
 
 char *** solveNQueens(int n, int* returnSize, int** returnColumnSizes){
     char ***retQ = NULL;
-    int *rcs = NULL;
+    unsigned char *rcs = NULL;
+    unsigned char steps[10] = {0};
 
     qsn = 0;
-    findQSolution()
+
+    findQSolution(steps, n, 0);
+
+    if (qsn == 0) {
+        *returnSize = 0;
+        *returnColumnSizes = NULL;
+        return NULL;
+    }
 
     //设置好返回值
     rcs = malloc(qsn * sizeof(int));
@@ -51,9 +88,10 @@ char *** solveNQueens(int n, int* returnSize, int** returnColumnSizes){
             return NULL;
         }
 
+        memset(sc, '.', n*n);
         for (int j = 0; j < n; j++) {
             solution[j] = &sc[n*j];
-            memcpy(&sc[n*j], qs[i][j], n);
+            sc[n*j + qs[i][j]] = 'Q';
         }
 
         rcs[i] = n;
