5d70cf24fc8f142f0406393b37b76273342db8c2
whuaguo <whuaguo@gmail.com>
2021-09-30 19:56:51 +0800

    c

--- 75.颜色分类.c
+++ 75.颜色分类.c
@@ -9,15 +9,20 @@ void sortColors(int* nums, int numsSize){
     int last0 = 0, left = 0, right = numsSize - 1;
     while (left <= right) {
         if(nums[left] == 0){
-            nums[left] = nums[last0];
-            nums[last0] = 0;
-            last0++;
-            left++;
+            nums[left++] = 1;
+            nums[last0++] = 0;
         } else if(nums[left] == 2){
+            //找到第一个非2
+            while (nums[right] == 2 && (right > left)) {
+                right --;
+            }
+
+            //交换一下
             nums[left] = nums[right];
             nums[right] = 2;
             right--;
         } else {
+            //移动
             left++;
         }
     }
