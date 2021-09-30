575755383f6ba133ace59f71d851320498a7f7ad
whuaguo <whuaguo@gmail.com>
2021-09-30 15:55:33 +0800

    c

---
+++ 68.文本左右对齐.c
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=68 lang=c
+ *
+ * [68] 文本左右对齐
+ */
+
+// @lc code=start
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char ** fullJustify(char ** words, int wordsSize, int maxWidth, int* returnSize){
+
+}
+// @lc code=end
+
