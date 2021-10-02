5ddfaac255babb4faab3c96b3c4be87bd2c5999d
whuaguo <whuaguo@gmail.com>
2021-10-02 16:26:09 +0800

    c

---
+++ 39.组合总和.c
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=39 lang=c
+ *
+ * [39] 组合总和
+ */
+
+// @lc code=start
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** combinationSum(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes){
+
+}
+// @lc code=end
+
