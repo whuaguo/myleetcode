064fb46d75571ae40e248a22bce118d845ec37a1
whuaguo <whuaguo@gmail.com>
2021-10-14 10:36:32 +0800

    c

--- 127.单词接龙.c
+++ 127.单词接龙.c
@@ -26,160 +26,93 @@ int strDiffs(char * s1, char * s2) {
     return diffs;
 }
 
-#define INVALID_IDX     (-1)
-#define TARGET_IDX      (wordListSize)
-#define BEGIN_IDX       (wordListSize + 1)
-#define END_IDX         (wordListSize + 2)
-#define idx2word(idx)    ((idx == END_IDX) ? endWord : ((idx == BEGIN_IDX)? beginWord:wordList[idx]))
+#define INVALID_IDX     -1
+#define BEGIN_IDX       wordListSize
+#define END_IDX         wordListSize + 1
+#define TARGET_IDX      wordListSize + 2
+#define idx2word(idx)   ((idx == BEGIN_IDX) ? beginWord : wordList[idx])
 
 int ladderLength(char * beginWord, char * endWord, char ** wordList, int wordListSize){
-#if 1
-    short nextToEnd[5000] = {0};
-    short nextToStart[5000] = {0};
+    short nextToEnd[5000] = {0};   //path to end
+    short nextToStart[5000] = {0}; //path to begin
 
-    short idxs1[1000] = {0};
+    short idxs1[1000] = {0};        //用来存储从begin，end开始的集合，还有下一个迭代的集合
     short idxs2[1000] = {0};
     short idxs3[1000] = {0};
+    short *eidxs = idxs1;           //从begin开始
+    short *sidxs = idxs2;           //从end开始
+    short *expandIdxs = idxs3;      //下一次迭代
 
-    short *eidxs = idxs1;
-    short *sidxs = idxs2;
-    short *expandIdxs = idxs3;
-    short eidxsCount = 1;
-    short sidxsCount = 1;
-    short expandIdxsCount;
+    short eidxsCount = 0;           //刚开始只有endword
+    short edepth = 2;               //eidxs里面的是endword的邻居
 
-    short edepth = 0;
-    short sdepth = 0;
-
-    //判断endword是否在wordList中
-    int idx;
-    for (idx = 0; idx < wordListSize; idx++) {
-        if (strDiffs(wordList[idx], endWord) == 0) {
-            break;
+    memset(nextToEnd, INVALID_IDX, 5000*sizeof(short));
+    //判断endword是否在wordList中,顺便把endWord的邻居放入eidxs集合里面
+    for (int idx = 0; idx < wordListSize; idx++) {
+        int diffs = strDiffs(wordList[idx], endWord); 
+        if ( diffs == 0) {
+            nextToEnd[idx] = TARGET_IDX;
+            nextToEnd[TARGET_IDX] = idx;
+        } else if (diffs == 1) {
+            eidxs[eidxsCount++] = idx;
+            nextToEnd[idx] = END_IDX;
         }
     }
 
-    if (idx == wordListSize) {
+    if (nextToEnd[TARGET_IDX] == INVALID_IDX) {
+        //endword不在wordList里面，直接返回0
         return 0;
     }
 
-    memset(nextToEnd, INVALID_IDX, 5000*sizeof(short));
+    bool isNeighbor = (strDiffs(beginWord, endWord) == 1);
+    if (isNeighbor) {
+        return 2;
+    }
+
+    short sidxsCount = 1;           //刚开始只有beginword
+    short sdepth = 1;               //只有beginword自己
+    sidxs[0] = BEGIN_IDX;
     memset(nextToStart, INVALID_IDX, 5000*sizeof(short));
 
-    eidxs[0] = END_IDX;
-    sidxs[0] = BEGIN_IDX;
-
-    while (eidxsCount||sidxsCount) {
-        bool    is2end = (eidxsCount <= sidxsCount); 
-        short   idxsCount = is2end ? eidxsCount: sidxsCount;
-        short*  idxs = is2end ? eidxs : sidxs;
+    while (eidxsCount && sidxsCount) {
+        bool    is2end = (eidxsCount && (eidxsCount < sidxsCount)); 
+        short*  idxsCount = is2end ? &eidxsCount: &sidxsCount;
+        short*  depth = is2end ? &edepth: &sdepth;
         short*  paths = is2end ? nextToEnd : nextToStart;
         short*  rpaths = is2end ? nextToStart: nextToEnd;
-
-        if (is2end) {
-            edepth++;
-        } else {
-            sdepth++;
-        }
+        short** idxs = is2end ? &eidxs : &sidxs;
 
         int expandCount = 0;
-        for (int idx = 0; idx < idxsCount; idx++) {
-            char *targetWord = idx2word(idxs[idx]);
+        for (int idx = 0; idx < *idxsCount; idx++) {
+            char *targetWord = idx2word((*idxs)[idx]);
 
             for (int i = 0; i < wordListSize; i++) {
                 if (INVALID_IDX != paths[i]) {
                     continue;
                 } 
 
-                if (1 == strDiffs(targetWord, wordList[i])) {
-                    paths[i] = idxs[idx];
-                    expandIdxs[expandCount++] = i;
+                int diffs = strDiffs(targetWord, wordList[i]); 
+                if (1 == diffs) {
                     if (INVALID_IDX != rpaths[i]) {
-                        //meet
-                        return edepth + sdepth + 1;
+                        return edepth + sdepth;
                     }
+                    paths[i] = (*idxs)[idx];
+                    expandIdxs[expandCount++] = i;
                 }
             }
         }
 
         short *buffer = expandIdxs;
-        if (is2end) {
-            eidxsCount = expandCount;
-            expandIdxs = eidxs;
-            eidxs = buffer;
-        } else {
-            sidxsCount = expandCount;
-            expandIdxs = sidxs;
-            sidxs = buffer;
+        expandIdxs = *idxs;
+        *idxs = buffer;
+        *idxsCount = expandCount;
+
+        if (expandCount) {
+            (*depth)++;
         }
     }
 
     return 0;
-#else
-    short nextToEnd[5000] = {0};
-    short idxs1[1000] = {0};
-    short idxs2[1000] = {0};
-    short *idxs = idxs1;
-    short *expandIdxs = idxs2;
-    short idxsCount = 0;
-    short edepth = 0;
-
-    memset(nextToEnd, INVALID_IDX, 5000*sizeof(short));
-
-    //nextToEnd[],里面存放wordList里面向endword变化的下一个变种
-    //设定endword放在wordList[wordListSize]，beginword的下一个变种可放在一个全局变量。
-    for (int idx = 0; idx < wordListSize; idx++) {
-        int diffs = strDiffs(endWord, wordList[idx]);
-        if (1 == diffs ) {
-            nextToEnd[idx] = END_IDX; 
-            idxs[idxsCount++] = idx;
-        } else if (0 == diffs) {
-            nextToEnd[idx] = TARGET_IDX;
-            nextToEnd[TARGET_IDX] = idx;
-        }
-    }
-
-    if (INVALID_IDX == nextToEnd[TARGET_IDX]) {
-        return 0;
-    } 
-
-    if (1 == strDiffs(beginWord, endWord)) {
-        return 2;
-    }
-
-    edepth = 2;
-    while (idxsCount) {
-        int sidx;
-        int expandCount = 0;
-        edepth++;
-        
-        for (int idx = 0; idx < idxsCount; idx++) {
-            sidx = idxs[idx];
-
-            if (1 == strDiffs(wordList[sidx], beginWord)) {
-                return edepth;
-            }
-
-            for (int i = 0; i < wordListSize; i++) {
-                if (INVALID_IDX != nextToEnd[i]) {
-                    continue;
-                } 
-
-                if (1 == strDiffs(wordList[sidx], wordList[i])) {
-                    nextToEnd[i] = sidx;
-                    expandIdxs[expandCount++] = i;
-                }
-            }
-        }
-
-        idxsCount = expandCount;
-        short *buffer = idxs;
-        idxs = expandIdxs;
-        expandIdxs = buffer;
-    }
-
-    return 0;
-#endif
 }
 // @lc code=end
 
