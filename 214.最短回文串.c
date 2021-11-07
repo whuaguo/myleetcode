85e8d2546bec37d2e16492bafcd4b6d03a06b0d9
whuaguo <whuaguo@gmail.com>
2021-11-07 23:00:10 +0800

    c

---
+++ 214.最短回文串.c
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=214 lang=c
+ *
+ * [214] 最短回文串
+ */
+
+// @lc code=start
+
+
+char * shortestPalindrome(char * s){
+    int len = strlen(s);
+    int tlen = len << 1;
+    char *ts = malloc(tlen + 1);
+    assert(ts);
+
+    memcpy(ts, s, len);
+    ts[len] = '+';
+    for (int i = 0; i < len ; i++) {
+        ts[len + 1 + i] = s[len - 1 - i];
+    }
+
+    //total char tlen + 1
+    short *next = malloc((tlen + 3)*sizeof(short));
+    assert(next);
+
+    int i = 1;
+    int j = 0;
+    next[1] = 0;
+    while (i <= tlen+1) {
+        if ((j == 0) || (ts[i - 1] == ts[j - 1])) {
+            next[++i] = ++j;
+        } else {
+            j = next[j];
+        }
+    }
+
+    int rn = next[tlen+2] - 1;
+    free(next);
+
+    char * ret = malloc(tlen - rn + 1);
+    assert(ret);
+    
+    if (rn < len) {
+        memcpy(ret, ts+len+1, len - rn);
+    }
+    memcpy(ret + len -rn, s, len + 1);
+
+    free(ts);
+    return ret;
+}
+// @lc code=end
+
