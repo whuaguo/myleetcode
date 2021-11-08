465a0e86a1cf1340497dc6ca13bd6c11680016a9
whuaguo <whuaguo@gmail.com>
2021-11-08 08:08:52 +0800

    c

--- 214.最短回文串.c
+++ 214.最短回文串.c
@@ -9,17 +9,23 @@
 
 char * shortestPalindrome(char * s){
     int len = strlen(s);
-    int tlen = (len << 1) + 1;
-    char *ts = malloc(tlen);
-    assert(ts);
 
+    //复合字符串长度len * 2 + 1, 中间插入特殊字符串“#”。
+    //必须添加特殊字符串，否则"aaaa"这样的字符串，正反都一样，合在一起
+    //按照KMP字符串算next值的时候，最后一位的值是7，不是我们希望的4
+    int tlen = (len << 1) + 1;
+
+    //构造复合字符串，原始字符串+‘#’+翻转字符串
+    char *ts = malloc(tlen); 
+    assert(ts);
     memcpy(ts, s, len);
-    ts[len] = '+';
+    ts[len] = '#';
     for (int i = 0; i < len ; i++) {
         ts[len + 1 + i] = s[len - 1 - i];
     }
 
-    //total char tlen + 1
+    //next数组的长度是，n+2，下标x计算前面x-1个字符的next值，长度为n
+    //的字符串，最后一个next值的下标是n+1，那么数组的长度就是n+2
     short *next = malloc((tlen + 2)*sizeof(short));
     assert(next);
 
