bccd453bb5ecdeb6ea9fe3f746b185437984b69b
whuaguo <whuaguo@gmail.com>
2021-11-22 23:22:54 +0800

    c

---
+++ 216.组合总和-iii.c
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=216 lang=c
+ *
+ * [216] 组合总和 III
+ */
+
+// @lc code=start
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** combinationSum3(int k, int n, int* returnSize, int** returnColumnSizes){
+
+}
+// @lc code=end
+
