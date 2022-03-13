3ba03e0280b2087a910d5f2be38190bb90556f5d
whuaguo <whuaguo@gmail.com>
2022-03-13 20:06:29 +0800

    c

---
+++ 77.组合.c
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=77 lang=c
+ *
+ * [77] 组合
+ */
+
+// @lc code=start
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** combine(int n, int k, int* returnSize, int** returnColumnSizes){
+
+}
+// @lc code=end
+
