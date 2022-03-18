e05103c4e6b274bb1e38da265a7f7de9895f3023
whuaguo <whuaguo@gmail.com>
2022-03-18 19:23:49 +0800

    c

--- 220.存在重复元素-iii.c
+++ 220.存在重复元素-iii.c
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-//#define MAP 1
+#define MAP 1
 #define QSORT 1
 
 int intCompare(const void *a, const void *b)
@@ -19,36 +19,116 @@ typedef struct node
     int value;
 } mapNode;
 
-int mapNodeCompare(const void *a, const void *b)
-{
-    return (*(int *)a > *(int *)b);
-}
-
+#define mapNodeCompare intCompare
 #define absCompare(a, b, t) ((a & 0x80000000) ^ (b & 0x80000000)) ? (a <= b + t) : ((a - b) <= t)
+#define getIdx(u, size) (((size) < 0) ? (((u) >= 0) ? 0 : -1) : (((u) >= 0) ? ((u) / (size)) : ((((u) + 1) / (size)) - 1)))
+#define FREEMEM      \
+    {                \
+        free(lists); \
+        free(idxs);  \
+    }
+
+bool mapNodeAbsCompare(mapNode *lists, int idx, int t)
+{
+    return (lists[idx + 1].idx == (lists[idx].idx + 1)) ? absCompare(lists[idx + 1].value, lists[idx].value, t) : false;
+}
 
 bool containsNearbyAlmostDuplicate(int *nums, int numsSize, int k, int t)
 {
-#if (defined MAP)
-#elif (defined QSORT)
     if (k == 0)
         return false;
 
-    if (t == 0)
+#if (defined MAP)
+    int size = (k < numsSize) ? (k + 1) : numsSize;
+    int dis = (t == 0x7fffffff) ? -1 : (t + 1);
+
+    int *idxs = malloc(sizeof(int) * numsSize);
+    mapNode *lists = malloc(sizeof(mapNode) * (size));
+    assert(idxs != NULL);
+    assert(lists != NULL);
+
+    for (int idx = 0; idx < size; idx++)
     {
-        for (int idx = 1; idx < numsSize; idx++)
-        {
-            int fromidx = (idx > k) ? (idx - k) : 0;
-            for (int i = fromidx; i < idx; i++)
-            {
-                if (nums[i] == nums[idx])
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
+        idxs[idx] = getIdx(nums[idx], dis);
+        lists[idx].idx = idxs[idx];
+        lists[idx].value = nums[idx];
     }
 
+    qsort(lists, size, sizeof(mapNode), mapNodeCompare);
+
+    for (int idx = 0; idx < size - 1; idx++)
+    {
+        if (lists[idx].idx == lists[idx + 1].idx || mapNodeAbsCompare(lists, idx, t))
+        {
+            FREEMEM;
+            return true;
+        }
+    }
+
+    mapNode *end = lists + k;
+    for (int idx = size; idx < numsSize; idx++)
+    {
+        mapNode *item = bsearch(&idxs[idx - size], lists, size, sizeof(mapNode), intCompare);
+
+        int newIdx = getIdx(nums[idx], dis);
+        if (item->idx != newIdx)
+        {
+            if (bsearch(&newIdx, lists, size, sizeof(mapNode), intCompare))
+            {
+                FREEMEM;
+                return true;
+            }
+
+            while (item > lists)
+            {
+                if ((*(item - 1)).idx == newIdx)
+                {
+                    FREEMEM;
+                    return true;
+                }
+
+                if ((*(item - 1)).idx < newIdx)
+                    break;
+
+                memcpy(item, item - 1, sizeof(mapNode));
+                item--;
+            }
+
+            while (item < end)
+            {
+                if ((*(item + 1)).idx == newIdx)
+                {
+                    FREEMEM;
+                    return true;
+                }
+
+                if ((*(item + 1)).idx > newIdx)
+                    break;
+
+                memcpy(item, item + 1, sizeof(mapNode));
+                item++;
+            }
+        }
+
+        idxs[idx] = newIdx;
+        item->idx = newIdx;
+        item->value = nums[idx];
+        if ((item != lists) && (absCompare(nums[idx], (*(item - 1)).value, t)))
+        {
+            FREEMEM;
+            return true;
+        }
+
+        if ((item != end) && (absCompare((*(item + 1)).value, nums[idx], t)))
+        {
+            FREEMEM;
+            return true;
+        }
+    }
+
+    FREEMEM;
+    return false;
+#elif (defined QSORT)
     int size = (k < numsSize) ? (k + 1) : numsSize;
     int *lists = malloc(sizeof(int) * size);
     assert(lists != NULL);
@@ -87,10 +167,10 @@ bool containsNearbyAlmostDuplicate(int *nums, int numsSize, int k, int t)
             item = item + 1;
         }
 
-        bool left = (item == lists) ? false : (absCompare(new, *(item - 1), t));
-        bool right = (item == (lists + k)) ? false : (absCompare(*(item + 1), new, t));
+        if ((item != lists) && (absCompare(new, *(item - 1), t)))
+            return true;
 
-        if (left || right)
+        if ((item != (lists + k)) && (absCompare(*(item + 1), new, t)))
             return true;
 
         *item = new;
@@ -98,14 +178,11 @@ bool containsNearbyAlmostDuplicate(int *nums, int numsSize, int k, int t)
 
     return false;
 #else
-    if (numsSize <= 1)
-        return false;
-
-    for (int idx = 1; idx < numsSize; idx++)
+    if (t == 0)
     {
-        int fromidx = (idx > k) ? (idx - k) : 0;
-        if (!t)
+        for (int idx = 1; idx < numsSize; idx++)
         {
+            int fromidx = (idx > k) ? (idx - k) : 0;
             for (int i = fromidx; i < idx; i++)
             {
                 if (nums[i] == nums[idx])
@@ -114,18 +191,21 @@ bool containsNearbyAlmostDuplicate(int *nums, int numsSize, int k, int t)
                 }
             }
         }
-        else
-        {
-            long min, max;
-            min = (long)nums[idx] - t;
-            max = (long)nums[idx] + t;
+        return false;
+    }
 
-            for (int i = fromidx; i < idx; i++)
+    for (int idx = 1; idx < numsSize; idx++)
+    {
+        int fromidx = (idx > k) ? (idx - k) : 0;
+        long min, max;
+        min = (long)nums[idx] - t;
+        max = (long)nums[idx] + t;
+
+        for (int i = fromidx; i < idx; i++)
+        {
+            if (nums[i] >= min && nums[i] <= max)
             {
-                if (nums[i] >= min && nums[i] <= max)
-                {
-                    return true;
-                }
+                return true;
             }
         }
     }
