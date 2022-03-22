a86a5092a9e87a054e4a4f98dfca923f203f2deb
whuaguo <whuaguo@gmail.com>
2022-03-22 19:11:44 +0800

    c

--- 273.整数转换英文表示.c
+++ 273.整数转换英文表示.c
@@ -6,46 +6,49 @@
 
 // @lc code=start
 
-char num2str_small[20][20] = {
+char num2str_small[20][10] = {
     "Zero",
     "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
     "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-char num2str_medium[10][20] = {
+char num2str_medium[10][10] = {
     "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-char num2str_large[4][20] = {
+char num2str_large[4][10] = {
     "Billion",
     "Million",
     "Thousand",
     "",
 };
 
+#define appendStr(ret, str)   \
+    {                         \
+        if (strlen(ret))      \
+        {                     \
+            strcat(ret, " "); \
+        }                     \
+        strcat(ret, str);     \
+    }
+
 void num2str(char *ret, int num)
 {
     if (num >= 100)
     {
-        strcat(ret, num2str_small[num / 100]);
-        strcat(ret, " Hundred");
+        //百位
+        appendStr(ret, num2str_small[num / 100]);
+        appendStr(ret, "Hundred");
+        num = num % 100;
     }
-    printf("ret: %s\n", ret);
 
-    num = num % 100;
     if (num >= 20)
     {
-        strcat(ret, " ");
-        strcat(ret, num2str_medium[num / 10]);
-        printf("ret: %s\n", ret);
-        if (num % 10)
-        {
-            strcat(ret, " ");
-            strcat(ret, num2str_small[num % 10]);
-            printf("ret: %s\n", ret);
-        }
+        //十位
+        appendStr(ret, num2str_medium[num / 10]);
+        num = num % 10;
     }
-    else if (num)
+
+    if (num)
     {
-        strcat(ret, " ");
-        strcat(ret, num2str_small[num]);
-        printf("ret: %s\n", ret);
+        //小于19直接
+        appendStr(ret, num2str_small[num]);
     }
 }
 
@@ -54,9 +57,33 @@ char *numberToWords(int num)
     bool start = false;
     char *ret = malloc(sizeof(char) * 500);
     assert(ret != NULL);
-
     memset(ret, 0, 500);
-    num2str(ret, num);
+
+    if (!num)
+    {
+        strcat(ret, num2str_small[0]);
+        return ret;
+    }
+
+    int base = 1000000000;
+    int idx = 0;
+    while (idx < 4)
+    {
+        int part = num / base;
+
+        if (part)
+        {
+            num2str(ret, part);
+            if (idx < 3)
+            {
+                appendStr(ret, num2str_large[idx]);
+            }
+        }
+
+        num = num % base;
+        base = base / 1000;
+        idx++;
+    }
 
     return ret;
 }
