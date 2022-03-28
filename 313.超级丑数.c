7cd9bce38555070f3b917c8224a1ed30093ad5ab
whuaguo <whuaguo@gmail.com>
2022-03-28 10:27:09 +0800

    c

--- 313.超级丑数.c
+++ 313.超级丑数.c
@@ -61,18 +61,32 @@ void logHeap(int *heap, int heapSize)
 
 void heapChangeHead(int *heap, int *heapSize, int newValue)
 {
-    log("Change head %d -> %d\n", heap[0], newValue);
+    // log("Change head %d -> %d\n", heap[0], newValue);
     heap[0] = newValue;
     heapMove(heap, *heapSize, 0);
 }
 
 void heapAddTail(int *heap, int *heapSize, int newValue)
 {
-    //log("Add %d to heap tail\n", newValue);
+    // log("Add %d to heap tail\n", newValue);
     heap[*heapSize] = newValue;
     *heapSize += 1;
     heapMove(heap, *heapSize, *heapSize - 1);
 }
+
+#define checkMaxChange(heapMax, newvalue, heapMaxChange) \
+    if (newvalue > heapMax)                              \
+    {                                                    \
+        heapMaxChange++;                                 \
+        heapMax = newvalue;                              \
+    }
+
+#define moveTailToHead(heap, heapSize)          \
+    {                                           \
+        int value = heap[heapSize - 1];         \
+        heapSize--;                             \
+        heapChangeHead(heap, &heapSize, value); \
+    }
 
 int nthSuperUglyNumber(int n, int *primes, int primesSize)
 {
@@ -82,46 +96,56 @@ int nthSuperUglyNumber(int n, int *primes, int primesSize)
     int heapMaxSize = n * primesSize;
     int *heap = malloc(sizeof(int) * heapMaxSize);
     assert(heap != NULL);
-
     heap[0] = 1;
+
+    int heapMax = 1;
+    int heapMaxChange = 1;
     while (urgCount < n)
     {
         int value = heap[0];
-        if ((value == prevNum) || (value > (0x7fffffff / primes[0])))
+        if (value == prevNum)
         {
-            if (value != prevNum)
-            {
-                urgCount++;
-                prevNum = value;
-            }
-            value = heap[heapSize - 1];
-            heapSize--;
-            heapChangeHead(heap, &heapSize, value);
+            moveTailToHead(heap, heapSize);
+            continue;
         }
-        else
-        {
-            urgCount++;
-            prevNum = value;
-            heapChangeHead(heap, &heapSize, value * primes[0]);
-            for (int i = 1; i < primesSize; i++)
-            {
-                if (value > (0x7fffffff / primes[i]))
-                    break;
 
-                int newvalue = value * primes[i];
-                if (heapSize > (n - urgCount) && (newvalue > heap[heapSize - 1]))
-                    break;
-                heapAddTail(heap, &heapSize, newvalue);
-            }
-            logHeap(heap, heapSize);
+        urgCount++;
+        prevNum = value;
+
+        if (value > (0x7fffffff / primes[0]))
+        {
+            moveTailToHead(heap, heapSize);
+            continue;
         }
+
+        int newvalue = value * primes[0];
+        if ((heapMaxChange > n) && (newvalue >= heapMax))
+        {
+            moveTailToHead(heap, heapSize);
+            continue;
+        }
+
+        checkMaxChange(heapMax, newvalue, heapMaxChange);
+        heapChangeHead(heap, &heapSize, newvalue);
+
+        for (int i = 1; i < primesSize; i++)
+        {
+            if (value > (0x7fffffff / primes[i]))
+                break;
+
+            newvalue = value * primes[i];
+            if ((heapMaxChange > n) && (newvalue >= heapMax))
+                break;
+
+            checkMaxChange(heapMax, newvalue, heapMaxChange);
+            heapAddTail(heap, &heapSize, newvalue);
+        }
+        // logHeap(heap, heapSize);
     }
 
     while (heap[0] == prevNum)
     {
-        int value = heap[heapSize - 1];
-        heapSize--;
-        heapChangeHead(heap, &heapSize, value);
+        moveTailToHead(heap, heapSize);
     }
 
     return heap[0];
