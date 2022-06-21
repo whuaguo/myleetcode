11e2dd6379a86773d86ada590f176f48713eb6a1
whuaguo <whuaguo@gmail.com>
2022-06-21 22:38:28 +0800

    c

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-void main() {
-    printf("hello, world!");
-    return;
+#define twice(x) (      \
+    {                   \
+        int y = x << 1; \
+        y;              \
+    })
+
+int main()
+{
+    printf("twice of 5 is %d!\n", twice(5));
+
+    return 0;
 }
