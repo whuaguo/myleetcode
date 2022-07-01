1821d30aaa3042a8c5e6fb9b4ea38ea3e46910d1
whuaguo <whuaguo@gmail.com>
2022-07-01 15:31:01 +0800

    c

---
+++ 99.恢复二叉搜索树.c
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=99 lang=c
+ *
+ * [99] 恢复二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool findAbnormalNode(struct TreeNode *root,
+                      struct TreeNode **pPrev,
+                      struct TreeNode **pNode1,
+                      struct TreeNode **pNode2)
+{
+    if (root->left)
+    {
+        //左子树寻找
+        if (findAbnormalNode(root->left, pPrev, pNode1, pNode2))
+        {
+            return true;
+        }
+    }
+
+    if (*pPrev)
+    {
+        if ((*pPrev)->val > root->val)
+        {
+            //前一个节点比当前节点大，必然是交换节点的情况
+            *pNode2 = root;
+            if (NULL == *pNode1)
+            {
+                //第一个节点为空，代表第一次碰到，那么前一个节点肯定是需要寻找的第一个节点。
+                *pNode1 = *pPrev;
+            }
+            else
+            {
+                //第一个节点不为空，那么root肯定是第二个节点，找到所有节点，返回
+                return true;
+            }
+        }
+    }
+    //设置前一个节点
+    *pPrev = root;
+
+    if (root->right)
+    {
+        //右子树寻找
+        if (findAbnormalNode(root->right, pPrev, pNode1, pNode2))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void recoverTree(struct TreeNode *root)
+{
+    struct TreeNode *pPrev = NULL;
+    struct TreeNode *pLeft = NULL;
+    struct TreeNode *pRight = NULL;
+
+    findAbnormalNode(root, &pPrev, &pLeft, &pRight);
+
+    int save = pLeft->val;
+    pLeft->val = pRight->val;
+    pRight->val = save;
+
+    return;
+}
+// @lc code=end
